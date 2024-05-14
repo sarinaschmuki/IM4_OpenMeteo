a58ee4c695472632db7f15f4ea8bc3794e66baa2
sarinaschmuki <sarina-schmuki@bluewin.ch>
2024-05-14 14:22:22 +0200

    changes jetzt funktioniert es wieder

--- index.php
+++ index.php
@@ -8,61 +8,84 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
-        <div class="wrapper">
-            <div class="container">
-      <!-- Logo Container in anderen Container einbinden -->
-      <div class="logo-container">
-        <img class="logo" src="Bilder/Logo_Pollenpuls.png"PollenPuls Logo>
-    </div>
-<h1>Pollenaktivität Schweiz</h1>
-    <h2>In welcher Umgebung heute wandern?</h2>
-    <select id="pollenTypeSelect" required>
-        <option value="" disabled selected>Pollenart auswählen</option>
-        <option value="alder_pollen">Erlenpollen</option>
-        <option value="birch_pollen">Birkenpollen</option>
-        <option value="grass_pollen">Graspollen</option>
-    </select>
-    <select id="dateSelect" required>
-        <option value="" disabled selected>Datum</option>
-    </select>
-    <div class="karte-container">
-        <!--<object class="karte" type="image/svg+xml" data="Bilder/Karte.svg" id="svgMap"></object>-->
-        <?php 
-            echo file_get_contents('Bilder/Karte.svg');
-        ?>
-    </div>
-    <div id="pollenInfo" class="pollen-info hidden">
-        <h2>Pollen Details</h2>
-        <p id="infoText"></p>
-    </div>
-    <br>
-</br>
-    <main> 
-          <!-- Legende für Pollenaktivität -->
-<div class="legend">
-    <div class="legend-item">
-        <span class="color-box" style="background-color: #AAAAAA;"></span>
-        <span class="description">Keine Pollenaktivität</span>
-    </div>
-    <div class="legend-item">
-        <span class="color-box" style="background-color: #C7E0C9;"></span>
-        <span class="description">Gering 1-25 no/m3</span>
-    </div>
-    <div class="legend-item">
-        <span class="color-box" style="background-color: #9AB59C;"></span>
-        <span class="description">Mittel 25-50 no/m3</span>
-    </div>
-    <div class="legend-item">
-        <span class="color-box" style="background-color: #586B5A;"></span>
-        <span class="description">Stark ab 50 no/m3</span>
-    </div>
-</div>
-    </main>
-    
-    <footer>
+    <div class="wrapper">
+        <div class="container">
+            <div class="logo-container">
+                <img class="logo" src="Bilder/Logo_Pollenpuls.png" alt="PollenPuls Logo">
+            </div>
+            <h1>Pollenaktivität Schweiz</h1>
+            <h2>Welche Umgebung heute zum Wandern meiden?</h2>
+            <div id="pollenTypeSelect">
+                <label><input type="radio" name="pollenType" value="alder_pollen" class="pollen-checkbox"> Erlenpollen</label>
+                <label><input type="radio" name="pollenType" value="birch_pollen" class="pollen-checkbox" checked> Birkenpollen</label>
+                <label><input type="radio" name="pollenType" value="grass_pollen" class="pollen-checkbox"> Graspollen</label>
+            </div>
 
-    </footer>
+            <!-- Date selection as a calendar input -->
+            <label for="dateSelect"></label>
+            <input type="date" id="dateSelect" required>
+
+            <!-- Hour selection -->
+            <label for="hourSelect"></label>
+            <select id="hourSelect">
+                <option value="00">00:00</option>
+                <option value="01">01:00</option>
+                <option value="02">02:00</option>
+                <option value="03">03:00</option>
+                <option value="04">04:00</option>
+                <option value="05">05:00</option>
+                <option value="06">06:00</option>
+                <option value="07">07:00</option>
+                <option value="08">08:00</option>
+                <option value="09">09:00</option>
+                <option value="10">10:00</option>
+                <option value="11">11:00</option>
+                <option value="12">12:00</option>
+                <option value="13">13:00</option>
+                <option value="14">14:00</option>
+                <option value="15">15:00</option>
+                <option value="16">16:00</option>
+                <option value="17">17:00</option>
+                <option value="18">18:00</option>
+                <option value="19">19:00</option>
+                <option value="20">20:00</option>
+                <option value="21">21:00</option>
+                <option value="22">22:00</option>
+                <option value="23">23:00</option>
+            </select>
+
+            <div class="karte-container">
+                <!-- SVG map -->
+                <?php echo file_get_contents('Bilder/Karte.svg'); ?>
+            </div>
+            <div id="pollenInfo" class="pollen-info hidden">
+                <h2>Pollenbelastung pro Kubikmeter</h2>
+                <p id="infoText"></p>
+            </div>
+            <main>
+                <!-- Legend for pollen activity -->
+                <div class="legend">
+                    <div class="legend-item">
+                        <span class="color-box" style="background-color: #AAAAAA;"></span>
+                        <span class="description">Keine Pollenaktivität</span>
+                    </div>
+                    <div class="legend-item">
+                        <span class="color-box" style="background-color: #C7E0C9;"></span>
+                        <span class="description">Gering 1-25 no/m3</span>
+                    </div>
+                    <div class="legend-item">
+                        <span class="color-box" style="background-color: #9AB59C;"></span>
+                        <span class="description">Mittel 25-50 no/m3</span>
+                    </div>
+                    <div class="legend-item">
+                        <span class="color-box" style="background-color: #586B5A;"></span>
+                        <span class="description">Stark ab 50 no/m3</span>
+                    </div>
+                </div>
+            </main>
+            <footer></footer>
+        </div>
+    </div>
     <script src="js/script.js"></script>
-    
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Asynchrone Funktion zum Abrufen von Daten
+// Asynchronous function to fetch data
 async function fetchData(url) {
   try {
     const response = await fetch(url);
@@ -6,47 +6,44 @@ async function fetchData(url) {
     return data;
   } catch (error) {
     console.error('Error fetching data:', error);
-    return null; // Return null to handle errors where the data is expected
+    return null;
   }
 }
 
-// Farben basierend auf Pollenanzahl definieren
+// Colors based on pollen count
 const pollenColors = {
-  0: '#AAAAAA',    // Keine Pollenaktivität
-  1: '#C7E0C9',    // Geringe Aktivität (1-25 no/m3)
-  25: '#9AB59C',   // Mittlere Aktivität (26-50 no/m3)
-  50: '#586B5A'    // Hohe Aktivität (>50 no/m3)
+  0: '#AAAAAA',    // No pollen activity
+  1: '#C7E0C9',    // Low activity (1-25 no/m3)
+  25: '#9AB59C',   // Moderate activity (26-50 no/m3)
+  50: '#586B5A'    // High activity (>50 no/m3)
 };
 
-// Variable, um den zuletzt ausgewählten Kanton zu speichern
+// Variable to store the last selected canton
 let lastSelectedKanton = null;
 
-// Funktion zum Einfärben der Karte basierend auf Pollendaten
+// Function to color the map based on pollen data
 async function colorMap(selectedDate, selectedPollenType) {
-  const url = 'https://374887-3.web.fhgr.ch/php/unload.php';
-  const data = await fetchData(url);
+  const selectedHour = document.getElementById('hourSelect').value;
+  const dateTime = `${selectedDate} ${selectedHour}:00:00`;
+  const url = `https://374887-3.web.fhgr.ch/php/unload.php?date=${encodeURIComponent(selectedDate)}&hour=${encodeURIComponent(selectedHour)}&pollenType=${encodeURIComponent(selectedPollenType)}`;
 
+  const data = await fetchData(url);
   if (!data) {
     console.error('No data available to color the map');
     return;
   }
 
   const svgDocument = document.querySelector('svg');
-  
-  // Sicherstellen, dass alle Kantone auf die Standardfarbe zurückgesetzt werden
   document.querySelectorAll('.st2').forEach(kantonElement => {
-    kantonElement.style.fill = pollenColors[0];
-    if (kantonElement.parentNode.querySelector('.st1')) {
-      kantonElement.parentNode.querySelector('.st1').style.stroke = "";
-      kantonElement.parentNode.querySelector('.st1').style.strokeWidth = "";
-    }
+    kantonElement.style.fill = pollenColors[0]; // Reset color
+    kantonElement.style.stroke = ""; // Reset outline
+    kantonElement.style.strokeWidth = "";
   });
 
   Object.keys(data).forEach(kanton => {
-    const kantonData = data[kanton][selectedDate] || {};
-    const pollenLevel = kantonData[selectedPollenType];
-
-    let color = pollenColors[0]; // Standardfarbe für keine Pollenaktivität
+    const kantonData = data[kanton];
+    const pollenLevel = kantonData[dateTime] && kantonData[dateTime][selectedPollenType];
+    let color = pollenColors[0]; // Default color
     if (pollenLevel > 50) {
       color = pollenColors[50];
     } else if (pollenLevel > 25) {
@@ -57,25 +54,71 @@ async function colorMap(selectedDate, selectedPollenType) {
 
     const kantonElement = svgDocument.getElementById(kanton);
     if (kantonElement) {
-      kantonElement.style.fill = color;
+      kantonElement.style.fill = color; // Apply color
     }
   });
 }
 
-// Event Listener für die Dropdowns zur Aktualisierung der Karte
-function updateMapFromDropdowns() {
+// Function to update the map from selected radio buttons and date/time
+function updateMapFromSelection() {
   const selectedDate = document.getElementById('dateSelect').value;
-  const selectedPollenType = document.getElementById('pollenTypeSelect').value;
-  colorMap(selectedDate, selectedPollenType);
+  console.log("Selected Date:", selectedDate);
+  const selectedPollenType = document.querySelector('.pollen-checkbox:checked').value;
+  if (selectedDate) {
+    colorMap(selectedDate, selectedPollenType);
+  }
 }
 
-document.getElementById('pollenTypeSelect').addEventListener('change', updateMapFromDropdowns);
-document.getElementById('dateSelect').addEventListener('change', updateMapFromDropdowns);
 
-// Funktion zum Füllen des Datum-Dropdowns basierend auf verfügbaren Daten
+document.querySelectorAll('.pollen-checkbox, #dateSelect, #hourSelect').forEach(element => {
+  element.addEventListener('change', updateMapFromSelection);
+});
+
+// Initialization of the application on load
+async function init() {
+  await fillDateDropdown();
+  updateMapFromSelection();
+}
+
+init();
+document.getElementById('pollenTypeSelect').addEventListener('change', updateMapFromSelection);
+document.getElementById('dateSelect').addEventListener('change', updateMapFromSelection);
+
+// Click event on kanton to show details
+document.querySelectorAll('.st2').forEach(kantonElement => {
+  kantonElement.addEventListener('click', async function() {
+    const kanton = this.id; // 'this' refers to kantonElement inside the event listener
+    const selectedDate = document.getElementById('dateSelect').value;
+    const selectedHour = document.getElementById('hourSelect').value;
+    const selectedPollenType = document.querySelector('.pollen-checkbox:checked').value;
+    const dateTime = `${selectedDate} ${selectedHour}:00:00`;
+    const url = `https://374887-3.web.fhgr.ch/php/unload.php?date=${encodeURIComponent(selectedDate)}&hour=${encodeURIComponent(selectedHour)}&pollenType=${encodeURIComponent(selectedPollenType)}`;
+
+    const data = await fetchData(url);
+
+    if (data && data[kanton] && data[kanton][dateTime]) {
+      const details = data[kanton][dateTime][selectedPollenType];
+      document.getElementById('infoText').innerText = `${details} no/m3`;
+      document.getElementById('pollenInfo').classList.remove('hidden');
+
+      if (lastSelectedKanton) {
+        const lastKantonElement = document.getElementById(lastSelectedKanton);
+        if (lastKantonElement) {
+          lastKantonElement.style.stroke = ""; // Reset previous outline
+          lastKantonElement.style.strokeWidth = "";
+        }
+      }
+
+      this.style.stroke = "#000000"; // Highlight new kanton
+      this.style.strokeWidth = "5";
+      lastSelectedKanton = kanton;
+    }
+  });
+});
+
+// Function to fill the date dropdown based on available data
 async function fillDateDropdown() {
   const data = await fetchData('https://374887-3.web.fhgr.ch/php/unload.php');
-  
   if (!data) {
     console.error('No data available to fill the date dropdown');
     return;
@@ -83,16 +126,16 @@ async function fillDateDropdown() {
 
   const dateSelect = document.getElementById('dateSelect');
   let uniqueDates = new Set();
-  
+
   Object.keys(data).forEach(kanton => {
-    Object.keys(data[kanton]).forEach(date => {
-      uniqueDates.add(date);
+    Object.keys(data[kanton]).forEach(datetime => {
+      const datePart = datetime.split('T')[0]; // Extract the date part from the datetime
+      uniqueDates.add(datePart);
     });
   });
 
   uniqueDates = Array.from(uniqueDates).sort();
-
-  dateSelect.innerHTML = ''; // Clear existing options
+  dateSelect.innerHTML = '<option value="" disabled selected>Datum wählen</option>';
   uniqueDates.forEach(date => {
     const option = document.createElement('option');
     option.value = date;
@@ -100,45 +143,8 @@ async function fillDateDropdown() {
     dateSelect.appendChild(option);
   });
 
-  if (uniqueDates.length > 0) {
+  /*if (uniqueDates.length > 0) {
     dateSelect.value = uniqueDates[0];
-    updateMapFromDropdowns(); // Initial update after filling the dropdown
-  }
+    updateMapFromSelection();
+  }*/
 }
-
-// Initialisierung der Anwendung beim Laden
-async function init() {
-  await fillDateDropdown();
-}
-
-init();
-
-// Funktion zum Anzeigen der Pollendetails beim Klicken auf einen Kanton
-document.querySelectorAll('.st2').forEach(kantonElement => {
-  kantonElement.addEventListener('click', async () => {
-    const kanton = kantonElement.id;
-    const selectedDate = document.getElementById('dateSelect').value;
-    const selectedPollenType = document.getElementById('pollenTypeSelect').value;
-    const data = await fetchData('https://374887-3.web.fhgr.ch/php/unload.php');
-
-    if (data && data[kanton] && data[kanton][selectedDate]) {
-      const details = data[kanton][selectedDate][selectedPollenType];
-      const infoText = details !== undefined ? `${details}` : 'Keine Daten verfügbar.';
-      document.getElementById('infoText').innerText = infoText;
-      document.getElementById('pollenInfo').classList.remove('hidden');
-
-      // Reset previous selected kanton's outline
-      if (lastSelectedKanton && document.getElementById(lastSelectedKanton)) {
-        const lastKantonPath = document.getElementById(lastSelectedKanton);
-        lastKantonPath.style.stroke = "";
-        lastKantonPath.style.strokeWidth = "";s
-      }
-
-      // Set new selected kanton outline
-      kantonElement.style.stroke = "#333"; // Schwarz
-      kantonElement.style.strokeWidth = "8"; // Dicke der Umrandung
-
-      lastSelectedKanton = kanton; // Update the last selected kanton
-    }
-  });
-});
